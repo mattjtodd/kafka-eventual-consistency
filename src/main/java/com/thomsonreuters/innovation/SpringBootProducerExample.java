fe81384e6b03b15a6f9d7560ee28cb352f934ba0
Pradeep <pradeep.sadashivamurthy@thomsonreuters.com>
2016-04-05 12:10:39 +0100

    Added UI

--- src/main/java/com/thomsonreuters/innovation/SpringBootProducerExample.java
+++ src/main/java/com/thomsonreuters/innovation/SpringBootProducerExample.java
@@ -6,12 +6,13 @@ import org.apache.kafka.clients.producer.Producer;
 import org.apache.kafka.clients.producer.ProducerRecord;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Import;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
 
+import javax.servlet.http.HttpServletRequest;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
@@ -23,13 +24,37 @@ import static javaslang.concurrent.Future.fromJavaFuture;
  */
 @Controller
 @EnableAutoConfiguration
+@ComponentScan("com.thomsonreuters.innovation")
 @Import({SimpleProducerExample.class, SimpleConsumerExample.class})
 public class SpringBootProducerExample {
 
-    @RequestMapping("/")
-    @ResponseBody
-    public String test() {
-        return "SpringBoot is ready!";
+    @RequestMapping(value = "/home", method = RequestMethod.GET)
+    public String home(Model model) {
+        CaseBean caseBean = new CaseBean();
+        model.addAttribute("caseBean", caseBean);
+        return "home";
+    }
+
+    @RequestMapping(value="/home", method=RequestMethod.POST)
+    public String produceMessage(@ModelAttribute CaseBean caseBean, Model model) {
+        String caseId = caseBean.getCaseId();
+
+        ExecutorService executorService = Executors.newCachedThreadPool();
+        try (Producer<String, String> producer = kafkaProducer()) {
+            Future
+                    .sequence(executorService, Stream
+                            .range(0, 1)
+                            .map(value -> new ProducerRecord<>("mytopic", "key ", caseId))
+                            .map(record -> fromJavaFuture(executorService, producer.send(record))))
+                    .onFailure(Throwable::printStackTrace)
+                    .onSuccess(sequence -> System.out.println("Completed Successfully, sent: " + sequence.size()))
+                    .await();
+        }
+        finally {
+            executorService.shutdown();
+        }
+        model.addAttribute("caseBean", caseBean);
+        return "outcome";
     }
 
     @RequestMapping("/message/case/{caseId}")
