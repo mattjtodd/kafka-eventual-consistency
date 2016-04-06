eaf3cd96cdbf2282819ed5a9eba208ab2f6f764e
Matt Todd <matt.todd@blackcatsolutions.co.uk>
2016-04-06 12:38:57 +0100

    Removed javaslang header

--- src/main/java/com/thomsonreuters/innovation/JpaCaseRepository.java
+++ src/main/java/com/thomsonreuters/innovation/JpaCaseRepository.java
@@ -1,8 +1,3 @@
-/*     / \____  _    _  ____   ______  / \ ____  __    _ _____
- *    /  /    \/ \  / \/    \ /  /\__\/  //    \/  \  / /  _  \   Javaslang
- *  _/  /  /\  \  \/  /  /\  \\__\\  \  //  /\  \ /\\/  \__/  /   Copyright 2014-now Daniel Dietrich
- * /___/\_/  \_/\____/\_/  \_/\__\/__/___\_/  \_//  \__/_____/    Licensed under the Apache License, Version 2.0
- */
 package com.thomsonreuters.innovation;
 
 import org.springframework.data.jpa.repository.JpaRepository;
